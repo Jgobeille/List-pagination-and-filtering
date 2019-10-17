f615c0f3de3535c01486fdfa8145eccbc4e37d20
Jamie Gobeille <djent34@gmail.com>
2019-10-17 17:08:08 -0400

    refactored and beautified

--- script.js
+++ script.js
@@ -5,8 +5,18 @@ FSJS project 2 - List Filter and Pagination
 
 document.addEventListener('DOMContentLoaded', () => {
    //set listItems to get all students
-   const studentList = Array.from(document.getElementsByClassName('student-item cf'));
+   const studentList = [...document.getElementsByClassName('student-item cf')];
    const numOfItemsPerPage = 10;
+   let newList = [];
+   const callFunctions = () => {
+      //run functions at page load
+      pageFunctions.addPaginationLinks(studentList);
+      pageFunctions.showPage(studentList, 1);
+      helperFunctions.activeClass();
+      pageFunctions.createSearchFunctionHTML();
+      helperFunctions.searchFunctionHandlers();
+   }
+
    const pageFunctions = {
       //function addPaginationLinks
       //Pass in list param
@@ -14,48 +24,62 @@ document.addEventListener('DOMContentLoaded', () => {
          helperFunctions.appendPaginationLinksToPage(list);
       },
       showPage: (list, page) => {
-         helperFunctions.displayPage(list, page);
+         helperFunctions.displayUpToTenItemsPerPage(list, page);
          helperFunctions.aTagClickHandler();
+      },
+      //refactor to make createElement a helper function
+      createSearchFunctionHTML: () => {
+         const pageHeader = document.getElementsByClassName('page-header')[0];
+         const div = helperFunctions.createDiv('student-search');
+         const input = document.createElement('input');
+         const button = document.createElement('button');
+         input.placeholder = 'Search for students...';
+         input.className = "input__search-input";
+         button.textContent = 'Search';
+         div.appendChild(input);
+         div.appendChild(button);
+         pageHeader.appendChild(div);
+
       }
    }
+
    //Alphabetized
    const helperFunctions = {
       aTagClickHandler: () => {
-         const a = [...document.getElementsByTagName('a')];    
-      a.forEach(a => {
-         a.addEventListener('click', e => {
-            pageFunctions.showPage(studentList, e.target.textContent)
-            e.target.className = 'active';   
+         const a = [...document.getElementsByTagName('a')];
+         a.forEach(a => {
+            a.addEventListener('click', e => {
+               pageFunctions.showPage(studentList, e.target.textContent)
+               e.target.className = 'active';
+            });
+            a.className = ' ';
          });
-         a.className = ' ';
-      });
       },
       //add class to first a tag
       activeClass: () => {
          const activeClass = document.getElementsByTagName('a');
          const arr = [...activeClass];
-               arr[0].className = 'active';
+         arr[0].className = 'active';
       },
       appendPaginationLinksToPage: (list) => {
          const numOfPages = Math.ceil(list.length / numOfItemsPerPage);
          //set page to get class page element
          const page = document.querySelector('.page');
          //set div to create div to page
-         const div = document.createElement('div');
+         const div = helperFunctions.createDiv('pagination');
          //set ul to create ul 
          const ul = document.createElement('ul');
-         //set div class to pagination
-         div.className ='pagination';
          //append ul to div
          div.appendChild(ul);
-         for(let i = 0; i < numOfPages; i++) {
+         for (let i = 0; i < numOfPages; i++) {
             helperFunctions.createPaginationLinks(ul, i);
          }
          //append div to page class element
-         page.appendChild(div); 
+         page.appendChild(div);
+
       },
       //loop over a elements and add clickHandler
-      createPaginationLinks: (ul,i) => {
+      createPaginationLinks: (ul, i) => {
          const li = document.createElement('li');
          const a = document.createElement('a');
          a.href = '#';
@@ -63,28 +87,60 @@ document.addEventListener('DOMContentLoaded', () => {
          li.appendChild(a);
          ul.appendChild(li);
       },
-      displayPage: (list, page) => {
+      createDiv: (className) => {
+         const div = document.createElement('div');
+         div.className = className;
+         return div;
+      },
+      displayUpToTenItemsPerPage: (list, page) => {
          //set startIndex to (page parameter * items per page) - items per page
          const startIndex = (page * numOfItemsPerPage) - numOfItemsPerPage;
          //set endIndex to page parameter * items per page
          const endIndex = page * numOfItemsPerPage
          //for each item in studentList
+
          list.forEach(listItem => {
             const index = (list.indexOf(listItem));
-            //if list item index(index meaning[0]) is greater than or equal to start index && less than index, display to page)
-            if(index >= startIndex && index < endIndex){
+            // pageFunctions.searchFunction(listItem);
+            //if list item index(index meaning[0]) is greater than or equal to start index && less than index, display to page
+            if (index >= startIndex && index < endIndex) {
                listItem.style.display = "";
             } else {
-               listItem.style.display ="none";
+               listItem.style.display = "none";
             }
          });
+      },
+      searchFunctionHandlers: () => {
+         const button = document.querySelector('button');
+         const searchInput = document.querySelector(".input__search-input");
+
+         searchInput.addEventListener('keyup', (e) => {
+            e.preventDefault();
+            helperFunctions.searchFunction(studentList);
+         });
+
+         button.addEventListener('click', e => {
+            e.preventDefault();
+            helperFunctions.searchFunction(studentList);
+         });
+      },
+      searchFunction: (list, page) => {
+         const searchInput = document.querySelector(".input__search-input");
+         list.forEach(listItem => {
+            if (searchInput.value.length !== 0 && listItem.textContent.toLowerCase().includes(searchInput.value.toLowerCase())) {
+               listItem.style.display = "";
+               newList.push(listItem);
+            } else {
+               listItem.style.display = "none";
+            }
+         });
+
+
+
       }
+
    }
-   //run functions at page load
-   pageFunctions.addPaginationLinks(studentList);
-   pageFunctions.showPage(studentList, 1);
-   helperFunctions.activeClass();
-   
+   callFunctions();
 });
 
 
